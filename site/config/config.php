ca3a34b9fe37a34854462699bde7bf9660fd7912
Romain <extravaganzatheone@gmail.com>
2018-12-08 12:22:26 +0100

    Auto stash before merge of "master" and "origin/master"

--- site/config/config.php
+++ site/config/config.php
@@ -2,6 +2,7 @@
 
 $rep = __DIR__ . '/../';
 
+<<<<<<< Updated upstream
 $vues['list'] = 'vues/list.php';
 $vues['error'] = 'vues/error.php';
 $vues['contact'] = 'vues/contact.php';
@@ -10,3 +11,9 @@ $vues['apropos'] = 'vues/apropos.php';
 
 $layout['head'] = 'vues/includes/head.php';
 $layout['footer'] = 'vues/includes/footer.php';
+=======
+$vues['list'] = 'views/list.php';
+$vues['error'] = 'views/error.php';
+$vues['contact'] = 'views/contact.php';
+$vues['apropos'] = 'views/apropos.php';
+>>>>>>> Stashed changes
