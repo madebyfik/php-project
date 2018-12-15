053a7cec29585c75d72ce0ad5a315cf6e26d76e2
Romain <extravaganzatheone@gmail.com>
2018-12-15 11:59:56 +0100

    Codage de Gateway Utilisateur et ListeTache

--- site/DAL/ListeTacheGateway.php
+++ site/DAL/ListeTacheGateway.php
@@ -14,10 +14,31 @@ class ListeTacheGateway {
         $this->_con->executeQuery($query,array(
             ':nom' => array($nom, PDO::PARAM_STR),
             ':public' => array($public, PDO::PARAM_INT),
-            ':idUtilisateur' => array($idUtilisateur, PDO::PARAM)
-        ))
-
+            ':idUtilisateur' => array($idUtilisateur, PDO::PARAM_INT)
+        ));
     }
-    public function update($id, $nom, $public, $idUtilisateur) {}
-    public function delete($id) {}
+
+
+    public function update($id, $nom, $public, $idUtilisateur) {
+        $query = 'UPDATE listetache SET nom=:nom, public=:public, idUtilisateur=:idUtilisateur WHERE id=:id';
+
+        $this->_con->executeQuery($query,array(
+            ':nom' => array($nom, PDO::PARAM_STR),
+            ':public' => array($public, PDO::PARAM_INT),
+            ':idUtilisateur' => array($idUtilisateur, PDO::PARAM_INT),
+            'id' => array($id, PDO::PARAM_INT)
+        ));
+    }
+
+
+    public function delete($id) {
+        $query = 'DELETE FROM listetache WHERE id=:id';
+
+        $this->_con->executeQuery($query, array(
+            'id' => array($id, PDO::PARAM_INT)
+        ));
+    }
+
+
 }
+
