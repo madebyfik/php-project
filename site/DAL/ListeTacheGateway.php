4df9abaa066e2f87a643c10a8631114d40d9c902
Romain <extravaganzatheone@gmail.com>
2018-12-15 11:22:26 +0100

    Codage des gatewaysss

--- site/DAL/ListeTacheGateway.php
+++ site/DAL/ListeTacheGateway.php
@@ -8,7 +8,16 @@ class ListeTacheGateway {
         $this->_con = $con;
     }
 
-    public function insert($nom, $public, $idUtilisateur) {}
+    public function insert($nom, $public, $idUtilisateur) {
+        $query = 'INSERT into listetache(nom, public, idUtilisateur) VALUES(:nom, :public, :idUtilisateur)';
+
+        $this->_con->executeQuery($query,array(
+            ':nom' => array($nom, PDO::PARAM_STR),
+            ':public' => array($public, PDO::PARAM_INT),
+            ':idUtilisateur' => array($idUtilisateur, PDO::PARAM)
+        ))
+
+    }
     public function update($id, $nom, $public, $idUtilisateur) {}
     public function delete($id) {}
 }
